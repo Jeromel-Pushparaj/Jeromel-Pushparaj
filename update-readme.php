36128651eda628155301d62d53536ed55b1fe7ac
Jeromel Pushparaj <jeromal03@gmail.com>
2025-05-01 13:26:33 +0530

    Update update-readme.php

--- update-readme.php
+++ update-readme.php
@@ -1,9 +1,9 @@
 <?php
-date_default_timezone_set('Asia/Kolkata'); // Set your timezone
+date_default_timezone_set('Asia/Kolkata');
 $readme = file_get_contents("README.md");
 
 $currentTime = date("Y-m-d H:i:s");
-$updated = preg_replace('/{{time}}/', $currentTime, $readme);
+$updated = preg_replace('/<time>.*<\/time>/', "<time>$currentTime</time>", $readme);
 
 file_put_contents("README.md", $updated);
 ?>
